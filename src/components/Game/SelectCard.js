f35e054e76535ddd75497e41d1e61abb1c838959
a.simonayan <a.simonyan@geniusee.com>
2022-02-07 12:51:40 +0400

    new commit

--- src/components/Game/SelectCard.js
+++ src/components/Game/SelectCard.js
@@ -5,6 +5,7 @@ import { AnimalImg, BackPage, Image, ImagesWrapper, ParentBlock } from "./Style"
 
 const SelectCard = () => {
     let avatar = []
+    let newArr = []
     const handleTurnImg = (ev) => {
         const isRotade = ev.target.classList.contains('rotate');
         if (isRotade) {
@@ -17,22 +18,20 @@ const SelectCard = () => {
     const handleImgeid = (name) => {
         avatar.push(name)
         if (avatar.length >= 2 && avatar[0] === avatar[1]) {
-            imageCards.data.filter(elem => elem.name !== avatar[1], console.log('lkkooooo'));
+            newArr = imageCards.data?.filter(elem => elem.name !== avatar[0], console.log('lkkooooo'));
+            console.log(avatar, 'hhhhhh')
+            console.log(newArr, 'llllll')
             avatar = []
-            console.log(imageCards.data, 'lll');
-
-            console.log('jisht e')
         } else if (avatar.length >= 2 && avatar[0] !== avatar[1]) {
             avatar = []
             console.log('sxal e')
         }
-
     }
 
     return (
         <ImagesWrapper>
             {
-                imageCards.data.map(item => {
+                imageCards.data.length >= 16 ? imageCards.data.map(item => {
                     return (
                         <ParentBlock
                             onClick={() => handleImgeid(item.name)}
@@ -45,6 +44,20 @@ const SelectCard = () => {
                         </ParentBlock>
                     )
                 })
+                    :
+                    newArr.map(item => {
+                        return (
+                            <ParentBlock
+                                onClick={() => handleImgeid(item.name)}
+                                key={item.id}>
+                                <AnimalImg >
+                                    <Image src={item.src} alt="png" />
+                                    <BackPage
+                                        onClick={handleTurnImg} />
+                                </AnimalImg>
+                            </ParentBlock>
+                        )
+                    })
             }
         </ImagesWrapper>
     )
